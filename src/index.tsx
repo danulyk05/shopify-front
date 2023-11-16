3a9093249ae6bbcb06373b82633877e52c0d066d
danulyk05 <ivan.danulyk2016@gmail.com>
2023-11-16 22:19:51 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AppProvider } from "@shopify/polaris";
+import enTranslations from "@shopify/polaris/locales/en.json";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider i18n={enTranslations}>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
 
